9f12a84507778d28377afe475fb1cfdbb68e3902
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2021-06-12 00:23:37 +0530

    fix

--- app/internal/helpers/ip_resolver.py
+++ app/internal/helpers/ip_resolver.py
@@ -7,11 +7,11 @@ def prod_ip_resolver(req: Request):
     cf = headers.get("x-real-ip") or headers.get("cf-connecting-ip")
     if cf:
         return cf
-    return headers.get("x-forwarded-for", req.client.host).split(",")[-1].strip()
+    return headers.get("x-forwarded-for", req.remote_addr).split(",")[-1].strip()
 
 
 def dev_ip_resolver(req: Request):
-    host = req.client.host
+    host = req.remote_addr
     print("[ip]", host)
     return host
 
